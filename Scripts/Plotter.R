11cb6a42cd3f76e048033394f2b587691a206bc8
Alanzegers <alanzegers@gmail.com>
2016-01-28 17:43:03 +0100

    legend added

--- Scripts/Plotter.R
+++ Scripts/Plotter.R
@@ -1,3 +1,6 @@
+# Alan Zegers & Stijn Wijdeven
+# january 2016
+
 plotter <- function(shp){
 	### Create colourscheme
 	palette <- c("#023858", "#045a8d", "#0570b0", "#3690c0", "#ef6548", "#d7301f", "#b30000", "#7f0000")
@@ -8,6 +11,12 @@ plotter <- function(shp){
 	leaflet() %>%
 		addProviderTiles("CartoDB.Positron") %>%
 		addPolygons(data = shp, stroke = FALSE, smoothFactor = 0.1, fillOpacity = 1,
-								color = ~pal(KLASSE))
+								color = ~pal(KLASSE))%>%
+
+		addLegend("bottomright", pal = pal, values = shp$KLASSE,
+							title = "low to high",
+							labFormat = labelFormat(prefix = ""),
+							opacity = 1
+		)
 	
 }
